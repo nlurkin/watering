9f3e1d3ee605482190e83f695d48a803d82e8dd0
Nicolas Lurkin <nicolas.lurkin@gmail.com>
2019-07-28 19:19:36 +0100

    Reorganize project structure
    Move Moisture into its own directory

--- Moisture/main.cpp
+++ Moisture/main.cpp
@@ -8,14 +8,18 @@
 #include <Arduino.h>
 #include "AutomaticWater.h"
 
-AutomaticWater waterSystem;
+//TODO add detection of sensor failure and permanently disable pump
+//TODO add detection of empty tank and permanently disable pump
+
+AutomaticWater waterSystem(22);
 
 void setup(){
    Serial.begin(115200);
 
    // Set tick at 1s - used for pump and sensor, not for display and buttons
    waterSystem.setTickInterval(1000);
-   waterSystem.addSensor(10, 40);
+   waterSystem.addCircuit(10, 50, 40);
+   waterSystem.addCircuit(11, 51, 41);
    waterSystem.initSystem();
 }
 
