dc9d9efa4819d133295f284d455cef466384b364
Nicolas Lurkin <nicolas.lurkin@gmail.com>
2019-07-28 23:41:36 +0100

    Finaly a main able to receive a request and send back acknowledgment and close the connection

--- Wifi/main.cpp
+++ Wifi/main.cpp
@@ -7,6 +7,7 @@
 
 #include <Arduino.h>
 #include "ESP8266Wifi.h"
+#include "HTTPRequest.h"
 
 ESP8266Wifi wifi;
 
@@ -14,25 +15,41 @@ void setup() {
 	Serial.begin(115200);
 	Serial1.begin(115200);
 
-	while(!wifi.checkBoardConnection()) delay(1000);
+	Serial.println("----- Arduino WIFI -----");
+	Serial.println("Checking ESP8266 connection...");
+	while (!wifi.checkBoardConnection())
+		delay(100);
 	Serial.println("Connection established");
 
 	wifi.restartBoard();
-	delay(5000);
+	delay(3000);
 
-	while(!wifi.checkWifiConnection()){
+	while (!wifi.checkWifiConnection()) {
 		wifi.connectWifi("WIFI_SSID", "WIFI_PASSWD");
 		delay(10000);
 	}
 	Serial.println("Connected to wifi");
+	if (wifi.startServer(80))
+		Serial.println("Server started on port 80");
 }
 
 void loop() {
-  if (Serial.available() > 0)
-  {
-	String command = Serial.readStringUntil('\n');
-	wifi.sendSomething(command);
-  }
+	if (Serial.available() > 0) {
+		String command = Serial.readStringUntil('\n');
+		wifi.sendSomething(command);
+	}
 
-  wifi.readAndPrint();
+	wifi.readAndPrint();
+
+	int8_t conn = wifi.payloadAvailable();
+	if (conn != -1) {
+		HTTPRequest http(wifi.getPayload(conn));
+		Serial.println("Got http payload");
+		http.print();
+		if(http.needs_answer()){
+			wifi.sendPacket("HTTP/1.1 200 OK\r\nContent-Type: application/text\r\nContent-Length: 10\r\n\r\n", conn);
+			wifi.sendPacket("Hey you!\r\n", conn);
+			wifi.closeConnection(conn);
+		}
+	}
 }
