f7a199449e10f3414b259a3c04a7d16b1ab332ce
nlurkin <nicolas.lurkin@cern.ch>
2019-08-02 16:18:34 +0100

    Implement NetworkStream class
    To be used like Serial. Supposed to send and receive data through the
    wifi network
    Untested so far

---
+++ Wifi/NetworkStream.cpp
@@ -0,0 +1,95 @@
+/*
+ * NetworkSerial.cpp
+ *
+ *  Created on: 2 Aug 2019
+ *      Author: Nicolas Lurkin
+ */
+
+#include "NetworkStream.h"
+
+#include "HTTPRequest.h"
+
+NetworkStream::NetworkStream(ESP8266Wifi &wifi, uint16_t port) :
+	_dest_port(0),
+	_rx_pos(0),
+	_rx_size(0),
+	_tx_pos(0),
+	_tx_size(0),
+	_wifi(wifi),
+	_server(wifi)
+{
+	_server.startServer(port);
+}
+
+NetworkStream::~NetworkStream() {
+}
+
+size_t NetworkStream::write(uint8_t v) {
+	return addChar(_tx_buffer, _tx_pos, _tx_size, NETWORK_TX_BUFFER_SIZE, v);
+}
+
+int NetworkStream::available() {
+	String data;
+	if(_wifi.payloadAvailable()>=0)
+		data = _server.loop();
+
+	int len = data.length();
+	size_t written = 1;
+	int pos=0;
+	while(pos<len && written>0)
+		written = addChar(_rx_buffer, _rx_pos, _rx_size, NETWORK_RX_BUFFER_SIZE, data[pos++]);
+
+	return _rx_size;
+}
+
+int NetworkStream::peek() {
+	return _rx_size == 0 ? -1 : _rx_buffer[_rx_pos];
+}
+
+int NetworkStream::read() {
+	if (_rx_size == 0) {
+		return -1;
+	} else {
+		int ret = _rx_buffer[_rx_pos];
+		_rx_pos++;
+		_rx_size--;
+		if (_rx_pos == NETWORK_RX_BUFFER_SIZE) {
+			_rx_pos = 0;
+		}
+		return ret;
+	}
+}
+
+int NetworkStream::availableForWrite() {
+	return NETWORK_TX_BUFFER_SIZE - _tx_size;
+}
+
+void NetworkStream::flush() {
+	HTTPRequest r = HTTPRequest::http_post();
+	String d;
+	d.reserve(_tx_size);
+	for(unsigned int i=0; i<_tx_size; ++i)
+		d+=_tx_buffer[i];
+	r.addContent(d);
+	int conn = _wifi.openConnection(_dest_address, _dest_port);
+	_wifi.sendPacket(r.generate(), conn);
+}
+
+void NetworkStream::setDestination(String address, uint16_t port) {
+	_dest_address = address;
+	_dest_port = port;
+}
+
+size_t NetworkStream::addChar(char *buffer, uint16_t &pos, uint16_t &size, uint16_t max_size, uint8_t v) {
+	if (size == max_size) {
+		return 0;
+	} else {
+		int p = pos + size;
+		if (p >= max_size) {
+			p -= max_size;
+		}
+		buffer[p] = v;
+		size++;
+		return 1;
+	}
+}
---
+++ Wifi/NetworkStream.h
@@ -0,0 +1,42 @@
+/*
+ * NetworkSerial.h
+ *
+ *  Created on: 2 Aug 2019
+ *      Author: Nicolas Lurkin
+ */
+
+#ifndef NETWORKSTREAM_H_
+#define NETWORKSTREAM_H_
+
+#include <Arduino.h>
+#include "ESP8266Wifi.h"
+#include "HTTPServer.h"
+
+class NetworkStream : public Stream {
+public:
+	static const uint16_t NETWORK_RX_BUFFER_SIZE = 64;
+	static const uint16_t NETWORK_TX_BUFFER_SIZE = 64;
+
+	NetworkStream(ESP8266Wifi &wifi, uint16_t port=80);
+	virtual ~NetworkStream();
+	virtual size_t write(uint8_t);
+	virtual int read();
+	virtual int available();
+	virtual int availableForWrite();
+	virtual int peek();
+	virtual void flush();
+
+	void setDestination(String address, uint16_t port);
+private:
+	static size_t addChar(char* buffer, uint16_t &pos, uint16_t &size, uint16_t max_size, uint8_t v);
+	uint16_t _dest_port;
+	uint16_t _rx_pos, _rx_size;
+	uint16_t _tx_pos, _tx_size;
+	String _dest_address;
+	ESP8266Wifi &_wifi;
+	HTTPServer _server;
+	char _rx_buffer[NETWORK_RX_BUFFER_SIZE];
+	char _tx_buffer[NETWORK_TX_BUFFER_SIZE];
+};
+
+#endif /* NETWORKSTREAM_H_ */
