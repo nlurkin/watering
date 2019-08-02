ae21a25c9506a7b59468fced147779c6115ec673
nlurkin <nicolas.lurkin@cern.ch>
2019-08-02 13:25:28 +0100

    Add second simple http server pretending to be an arduino
    This is for test purposes in case a working arduino is not at hand.
    It obeys the same protocol as will the arduino:
     - PUT: receive data from outside. This is in replacement of anything
    received on the Serial interface in case of using the NetworkSerial (not
    yet implemented)
     - GET: Doing nothing so far... Could be used to force the arduino to
    publish something?
    
    The others are not required
     - POST: Triggers server to send data outside. In the arduino the data
    send would be the replacement of any Serial.println() in case of
    NetworkSerial

---
+++ webserver/server2.py
@@ -0,0 +1,63 @@
+#!/bin/env python3
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import urllib.request
+
+
+class MyHandler(BaseHTTPRequestHandler):
+    data = ""
+
+    def print_it(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        self.data = post_data.decode("utf8")
+        print("== Received ==")
+        print(self.raw_requestline)
+        for header in self.headers:
+            print("> {0}:{1}".format(header, self.headers[header]))
+        print('-------------\n{0}\n-------------'.format(post_data))
+
+    def do_GET(self):
+        self.send_response(200, "OK")
+        self.end_headers()
+
+        self.print_it()
+        return
+
+    def publish(self, message):
+        message = bytes(message, "utf8")
+        url = "http://127.0.0.1:8000/"
+        print(message)
+        req = urllib.request.Request(url = url, data = message, method = 'POST')
+        with urllib.request.urlopen(req):
+            pass
+
+    def do_POST(self):
+        '''trigger Post output from arduino'''
+        self.send_response(200, "OK")
+        self.end_headers()
+
+        self.print_it()
+        message = "This is some random arduino output"
+        self.publish(message)
+
+        return
+
+    def do_PUT(self):
+        '''Received a command from server'''
+        self.send_response(200, "OK")
+        self.end_headers()
+
+        self.print_it()
+
+        if self.data == "command1":
+            self.publish("This is an answer to command1")
+        return
+
+
+def run(server_class = HTTPServer, handler_class = MyHandler):
+    server_address = ('', 8001)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+
+run()
