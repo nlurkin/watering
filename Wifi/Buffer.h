1a77e529994619e8592fba00bf4780b6990cdba4
Nicolas Lurkin <nicolas.lurkin@gmail.com>
2019-08-03 13:07:19 +0100

    Implement circular buffer class

---
+++ Wifi/Buffer.h
@@ -0,0 +1,37 @@
+/*
+ * Buffer.h
+ *
+ *  Created on: 3 Aug 2019
+ *      Author: Nicolas Lurkin
+ */
+
+#ifndef BUFFER_H_
+#define BUFFER_H_
+
+#include <Arduino.h>
+
+class Buffer {
+public:
+	Buffer(size_t size);
+	virtual ~Buffer();
+
+	void clear();
+
+	char peek();
+	inline size_t len();
+
+	size_t push(char c);
+	char read();
+	void get(char *dest, size_t max);
+	void get(char *dest, size_t max, char until);
+
+private:
+	void increment(char*& ptr, size_t len=1);
+	bool _allow_overwrite;
+	size_t _size;
+	char* _buffer;
+	char* _p_begin;
+	char* _p_end;
+};
+
+#endif /* BUFFER_H_ */
