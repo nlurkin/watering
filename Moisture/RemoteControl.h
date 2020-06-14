4d5f925d291f5fe75dba78c1406c15bea38a779e
Nicolas Lurkin <nicolas.lurkin@gmail.com>
2020-06-14 18:17:03 +0100

    Create RemoteControl class to implement moisture specific ControlServer

---
+++ Moisture/RemoteControl.h
@@ -0,0 +1,21 @@
+/*
+ * RemoteControl.h
+ *
+ *  Created on: 14 Jun 2020
+ *      Author: Nicolas Lurkin
+ */
+
+#ifndef REMOTECONTROL_H_
+#define REMOTECONTROL_H_
+
+#include "LibNetwork/ControlServer.h"
+/*
+ *
+ */
+class RemoteControl : public ControlServer {
+public:
+	RemoteControl(ESP8266Wifi &wifi);
+	virtual ~RemoteControl();
+};
+
+#endif /* REMOTECONTROL_H_ */
