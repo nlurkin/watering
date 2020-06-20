5877dac8f28c3c4ea224732c7eca4b615cc21d89
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2020-06-20 12:36:23 +0100

    Create dash waterapp

---
+++ webserver/waterdash/waterapp.py
@@ -0,0 +1,17 @@
+#!/bin/env python
+'''
+Created on 17 Jun 2020
+
+@author: Nicolas Lurkin
+'''
+
+import dash
+import dash_bootstrap_components as dbc
+from mongodb import myMongoClient
+
+# Initialise the app
+app = dash.Dash(__name__, external_stylesheets = [dbc.themes.BOOTSTRAP], suppress_callback_exceptions = True)
+server = app.server
+
+mongoClient = myMongoClient("localhost", 27017)
+mongoClient.connect()
