16e0fdaf768a1c47d34037c283f6bed1ebcde028
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2020-12-20 19:19:56 +0100

    Update main for test wifi server

--- Wifi/main.cpp
+++ Wifi/main.cpp
@@ -11,18 +11,25 @@
 #include <HardwareSerial.h>
 #include "LibESP8266/ESP8266Wifi.h"
 #include "LibNetwork/NetworkStream.h"
+#include "LibPublications/PubServer.h"
+#include "LibPublications/Publication.h"
 
 ESP8266Wifi wifi;
 
 #ifdef USE_NETWORK_STREAM
 NetworkStream mySerial(wifi);
 Stream &logSerial = mySerial;
+ControlServer myPubServer(wifi);
+Publication<double> pub_temperature("S1_T");
+Publication<int> pub_temperature1("S2_T");
 #else
 Stream &logSerial = Serial;
 #endif
 
 const char ssid[] PROGMEM = {""};
 const char pwd[]  PROGMEM = {""};
+
+unsigned long last_update;
 
 void setup() {
 	Serial.begin(115200);
@@ -48,6 +55,12 @@ void setup() {
 	mySerial.setDestination("192.168.1.20", 8000);
 	mySerial.begin(80);
 	#endif
+
+	myPubServer.setDestination("192.168.1.20", 8000);
+	myPubServer.addPublication(&pub_temperature);
+	myPubServer.addPublication(&pub_temperature1);
+
+	last_update = 0;
 }
 
 void loop() {
@@ -65,8 +78,18 @@ void loop() {
 	}
 	else if (logSerial.available() > 0) {
 		String command = logSerial.readStringUntil('\n');
-		wifi.sendSomething(command.c_str());
+		if(command.compareTo("report")){
+			mySerial.println("This is a report");
+		}
+		else
+			wifi.sendSomething(command.c_str());
 	}
 
+	if(millis()-last_update>5000){
+		pub_temperature.updateValue(5.2);
+		pub_temperature1.updateValue(10);
+		last_update = millis();
+	}
+	myPubServer.serve();
 	wifi.readAndPrint();
 }
