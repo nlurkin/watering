1d5289d9a452e2138b472f3d6be8766ab687581d
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2021-01-16 10:07:50 +0100

    Use MQTT for atmosphere

--- Atmosphere/main.cpp
+++ Atmosphere/main.cpp
@@ -11,15 +11,16 @@
 #include "MenuWelcome.h"
 #include "MenuBME.h"
 #include "ESP8266Wifi.h"
-#include "ControlServer.h"
+#include "MQTTControl.h"
 #include "BME280Reader.h"
 
 ESP8266Wifi wifi;
-ControlServer pubServer(wifi);
+MQTTClient mqtt(wifi, "arduino");
+MQTTControl pubServer(mqtt, "arduino");
 
 const char ssid[] = {""};
 const char pwd[]  = {""};
-const char serverIP[] = {""};
+const char serverHost[] = {""};
 
 LCDDisplay lcd(100);
 MenuWelcome _m_welcome(lcd.get_lcd_handle());
@@ -39,11 +40,13 @@ void setup() {
   lcd.add_menu(&_m_welcome);
   lcd.add_menu(&_m_bme);
 
+  mqtt.setUserPass("ardhome", "huY!rd89%");
+
   bme1.init(&_m_bme);
 
   Serial.println("Stating publication server");
-  pubServer.setDestination(serverIP, 8000);
-  pubServer.begin(80);
+  pubServer.setDestination(serverHost, 1883);
+  pubServer.begin();
 
   bme1.setPublicationServer(&pubServer);
 
@@ -54,6 +57,7 @@ void setup() {
   heartbeat_millis = millis();
 
   bme1.updateAll();
+  pubServer.advertise();
   pubServer.serve(true);
 }
 
