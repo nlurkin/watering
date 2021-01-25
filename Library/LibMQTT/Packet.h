16613698bc42364705446a5197b4d6c208486a82
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2021-01-25 09:44:26 +0100

    Increase MAX message length and payload length in MQTT
    This is necessary to contain the advertise service
    Also publication name length is increased

--- Library/LibMQTT/Packet.h
+++ Library/LibMQTT/Packet.h
@@ -36,9 +36,10 @@ struct VarHeader {
   uint8_t _bytes[128];
 };
 struct Payload {
+  static constexpr uint16_t MAX_PAYLOAD_LENGTH=700;
   Payload() : _n_bytes(0), _bytes{0} {};
-  uint8_t _n_bytes;
-  uint8_t _bytes[128];
+  uint16_t _n_bytes;
+  uint8_t _bytes[MAX_PAYLOAD_LENGTH];
 };
 
 class Packet {
--- Library/LibNetwork/PublicationBase.h
+++ Library/LibNetwork/PublicationBase.h
@@ -14,7 +14,7 @@
  */
 class PublicationBase {
 public:
-  static constexpr size_t MAX_NAME_LENGTH=20;
+  static constexpr size_t MAX_NAME_LENGTH=30;
 
   PublicationBase(const char *name);
   virtual ~PublicationBase();
