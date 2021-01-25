885bd2c7e26b8db95721bedf14829d2829f219a5
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2021-01-25 09:50:05 +0100

    Oregon setup receives data from up to MAX_SENSORS sensors
     - Upon first receiving, the rolling code is recorded in a "sighting"
    table.
     - When the same rolling code is received twice, the sensor is
    considered registerd and added to the list of known sensors.
     - The publications for the sensor are created and added to the control
    server.
     - An new "advertise" is sent

--- Atmosphere/OregonSetup.cpp
+++ Atmosphere/OregonSetup.cpp
@@ -10,12 +10,13 @@
 #include "OregonReader.h"
 
 OregonSetup::OregonSetup() :
+  _nsensors(0),
+  _sensors_list{0},
   _controlServer(nullptr),
   _oregon(new OregonReader),
-  _pub_temperature(nullptr),
-  _pub_humidity(nullptr)
+  _pub_temperature{nullptr},
+  _pub_humidity{nullptr}
 {
-
 }
 
 OregonSetup::~OregonSetup() {
@@ -25,11 +26,6 @@ void OregonSetup::setPublicationServer(ControlServer *server){
   _controlServer = server;
   if(server==nullptr)
     return;
-  // Create publications and add them
-  _pub_temperature = new Publication<double>("oregon_temperature");
-  _controlServer->addPublication(_pub_temperature);
-  _pub_humidity    = new Publication<int>("oregon_humidity");
-  _controlServer->addPublication(_pub_humidity);
 }
 
 bool OregonSetup::init(uint8_t pin) {
@@ -45,8 +41,93 @@ void OregonSetup::updateAll() {
     return;
 
   OregonDecoder sensor = _oregon->get_data();
+  if(!sensor.checksum())
+    return; // At least some data are invalid. Skip this one
+
+  uint8_t rolling_id = sensor.get_rolling_code();
+  int8_t index = isSensorKnown(rolling_id);
+
+  if(index<0 && _nsensors==MAX_SENSORS) // Nothing to do, we have no more space for a new sensor
+    return;
+
+  // In principle we have here either a known sensor, or space to add more
+  if(index==-99) { // First sighting
+    addSighting(rolling_id);
+    return; // Wait for at least another sighting to confirm its existence
+  }
+  else if(index<0) { // Second sighting. Add it to the registered sensors
+    index = addSensor(rolling_id, sensor, -index);
+    if(index==-1) // There has been a problem
+      return;
+  }
+
+  // Here we should definitely have an already registered sensor
   //sensor.print();
 
-  _pub_temperature->updateValue(sensor.get_temperature());
-  _pub_humidity->updateValue(sensor.get_relative_humidity());
+  if(!_controlServer)
+    return;
+
+  if(sensor.has_temperature())
+    _pub_temperature[index]->updateValue(sensor.get_temperature());
+  if(sensor.has_humidity())
+    _pub_humidity[index]->updateValue(sensor.get_relative_humidity());
+}
+
+int8_t OregonSetup::isSensorKnown(uint8_t id) {
+  for(uint8_t i=0; i<MAX_SENSORS; ++i){
+    if(_sensors_list[i]==id) // Look for registered sensors
+      return i;
+    if(_sensors_sightings[i]==id) // Look for sighted but unregistered sensors
+      return -i-1; // Add offset of 1, because 0==-0
+  }
+
+  return -99;
+}
+
+int8_t OregonSetup::addSighting(uint8_t id) {
+  for(uint8_t i=0; i<MAX_SENSORS; ++i){
+    if(_sensors_sightings[i]==0){
+      _sensors_sightings[i] = id; // Record it and return index in array
+      return i;
+    }
+  }
+  return -1; // Could not be added
+}
+
+int8_t OregonSetup::addSensor(uint8_t id, OregonDecoder &sensor, uint8_t sight_index) {
+  --sight_index; // Remove the offset of 1 previously added
+  if(sight_index>=MAX_SENSORS) // Not supposed to happen
+    return -1;
+  if(_nsensors>=MAX_SENSORS) // No more space for a sensor
+    return -1;
+
+  // Remove it from sightings
+  _sensors_sightings[sight_index] = 0;
+  // Add it to registered sensors and then increment number of registered sensors
+  uint8_t index = _nsensors++;
+  _sensors_list[index] = id;
+
+  // Add new publications
+  // Create publications and add them
+  char buffer[30];
+  uint16_t sensor_id = sensor.get_sensor_id();
+
+  if(!_controlServer)
+    return index;
+
+  if(sensor.has_temperature()) {
+    sprintf(buffer, "oregon_%04X_%02X_temperature", sensor_id, id);
+    _pub_temperature[index] = new Publication<double>(buffer);
+    _controlServer->addPublication(_pub_temperature[index]);
+  }
+  if(sensor.has_humidity()) {
+    sprintf(buffer, "oregon_%04X_%02X_humidity", sensor_id, id);
+    _pub_humidity[index]    = new Publication<int>(buffer);
+    _controlServer->addPublication(_pub_humidity[index]);
+  }
+
+  //Advertise new publications
+  _controlServer->advertise();
+
+  return index; // Return index of the sensor
 }
--- Atmosphere/OregonSetup.h
+++ Atmosphere/OregonSetup.h
@@ -9,12 +9,14 @@
 #define OREGONSETUP_H_
 
 #include "Publication.h"
+#include "OregonDecoder.h"
 
 class ControlServer;
 class OregonReader;
 
 class OregonSetup {
 public:
+  static constexpr uint8_t MAX_SENSORS = 5;
   OregonSetup();
   virtual ~OregonSetup();
 
@@ -24,10 +26,17 @@ public:
   void updateAll();
 
 private:
+  int8_t isSensorKnown(uint8_t id);
+  int8_t addSensor(uint8_t id, OregonDecoder &sensor, uint8_t sight_index);
+  int8_t addSighting(uint8_t id);
+
+  uint8_t              _nsensors;
+  uint8_t              _sensors_sightings[MAX_SENSORS];
+  uint8_t              _sensors_list[MAX_SENSORS];
   ControlServer       *_controlServer;
   OregonReader        *_oregon;
-  Publication<double> *_pub_temperature;
-  Publication<int>    *_pub_humidity;
+  Publication<double> *_pub_temperature[MAX_SENSORS];
+  Publication<int>    *_pub_humidity[MAX_SENSORS];
 };
 
 #endif /* OREGONSETUP_H_ */
