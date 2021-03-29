5b602d477adff014893ec2de2086e7b69ee69fa2
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2021-03-29 16:36:52 +0200

    Fix BME menu display

--- Atmosphere/MenuBME.cpp
+++ Atmosphere/MenuBME.cpp
@@ -25,16 +25,42 @@ MenuBME::~MenuBME() {
 }
 
 void MenuBME::init() {
-  _l_bme_header      = new LiquidLine(0, 0, "BME Measurement");
-  _l_bme_temperature = new LiquidLine(0, 1, "T: ", _bme_temperature, "*C");
+  char buffer[20], buffer2[5];
+  static const char header[] PROGMEM = "BME Measurement ";
+  static const char T[] PROGMEM = "T: ";
+  static const char TUnit[] PROGMEM = "*C";
+  static const char P[] PROGMEM = "P: ";
+  static const char PUnit[] PROGMEM = "hPa";
+  static const char H[] PROGMEM = "H: ";
+  static const char HUnit[] PROGMEM = "%";
+  static const char A[] PROGMEM = "A: ";
+  static const char AUnit[] PROGMEM = "m";
+
+  _l_bme_header      = new LiquidLine(0, 0, header);
+  _l_bme_temperature = new LiquidLine(0, 1, T, _bme_temperature, TUnit);
   _s_bme_temperature = new LiquidScreen(*_l_bme_header, *_l_bme_temperature);
-  _l_bme_pressure    = new LiquidLine(0, 1, "P: ", _bme_pressure, "hPa");
+  strcpy_P(buffer, PSTR("P: "));
+  strcpy_P(buffer2, PSTR("hPa"));
+  _l_bme_pressure    = new LiquidLine(0, 1, P, _bme_pressure, PUnit);
   _s_bme_pressure    = new LiquidScreen(*_l_bme_header, *_l_bme_pressure);
-  _l_bme_humidity    = new LiquidLine(0, 1, "H: ", _bme_humidity, "%");
+  strcpy_P(buffer, PSTR("H: "));
+  strcpy_P(buffer2, PSTR("%"));
+  _l_bme_humidity    = new LiquidLine(0, 1, H, _bme_humidity, HUnit);
   _s_bme_humidity    = new LiquidScreen(*_l_bme_header, *_l_bme_humidity);
-  _l_bme_altitude    = new LiquidLine(0, 1, "A: ", _bme_altitude, "m");
+  strcpy_P(buffer, PSTR("A: "));
+  strcpy_P(buffer2, PSTR("m"));
+  _l_bme_altitude    = new LiquidLine(0, 1, A, _bme_altitude, AUnit);
   _s_bme_altitude    = new LiquidScreen(*_l_bme_header, *_l_bme_altitude);
 
+  _l_bme_header->set_asProgmem(1);
+  _l_bme_temperature->set_asProgmem(1);
+  _l_bme_temperature->set_asProgmem(3);
+  _l_bme_pressure->set_asProgmem(1);
+  _l_bme_pressure->set_asProgmem(3);
+  _l_bme_humidity->set_asProgmem(1);
+  _l_bme_humidity->set_asProgmem(3);
+  _l_bme_altitude->set_asProgmem(1);
+  _l_bme_altitude->set_asProgmem(3);
   _menu.add_screen(*_s_bme_temperature);
   _menu.add_screen(*_s_bme_pressure);
   _menu.add_screen(*_s_bme_humidity);
