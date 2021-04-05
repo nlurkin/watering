aa2f76a8aba81691227e06273c4a4992c097453e
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2021-04-05 19:12:28 +0200

    Need to extract wifi parameters from flash memory in watchdog

--- Atmosphere/main.cpp
+++ Atmosphere/main.cpp
@@ -97,13 +97,19 @@ bool watchdog() {
         while(!wifi.checkCommunication(false))
           delay(10000);
         _m_welcome.communication(true);
-        watchdog_ok = wifi.init(ssid, pwd, true, false);
+        char p_ssid[30], p_pwd[30];
+        strcpy_P(p_ssid, ssid);
+        strcpy_P(p_pwd, pwd);
+        watchdog_ok = wifi.init(p_ssid, p_pwd, true, false);
         reset_failure = 0;
         if(watchdog_ok)
           pubServer.begin();
       }
       else { // No RESET at this point, just try to reconnect
-        wifi.connectWifi(ssid, pwd);
+        char p_ssid[30], p_pwd[30];
+        strcpy_P(p_ssid, ssid);
+        strcpy_P(p_pwd, pwd);
+        wifi.connectWifi(p_ssid, p_pwd);
         delay(1000);
         watchdog_ok = wifi.checkWifiConnection();
         if(!watchdog_ok)
