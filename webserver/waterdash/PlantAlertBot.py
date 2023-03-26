a22cdd2238f57d36b2244db5c228712e4b7129a7
Nicolas Lurkin <nicolas.lurkin@cern.ch>
2023-03-26 16:48:48 +0200

    Add checks on wind gusts

--- webserver/waterdash/PlantAlertBot.py
+++ webserver/waterdash/PlantAlertBot.py
@@ -80,16 +80,10 @@ def check_conditions(owm):
     if message is not None:
         message_list.append(message)
 
-    message = None
-    lower_0 = [_ for _ in temps if _[0]<0]
-    higher_30 = [_ for _ in temps if _[0]>30]
-    higher_20 = [_ for _ in temps if _[0]>20]
-    if len(lower_0):
-        message = f"WARNING: Temperatures below 0°C foreseen starting at {lower_0[0][1]}"
-    if len(higher_20):
-        message = f"WARNING: Temperatures above 20°C foreseen starting at {higher_20[0][1]}"
-    if len(higher_30):
-        message = f"WARNING: Temperatures above 30°C foreseen starting at {higher_30[0][1]}"
+    gusts = [(_[1].wind()["gust"], _[0]) for _ in hourly]
+    message = check_value_alerts(gusts, [], [30, 60], "Wind gusts", "kph")
+    if message is not None:
+        message_list.append(message)
 
     return message_list
 
