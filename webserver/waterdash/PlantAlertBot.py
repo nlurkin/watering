8a173c22f2a5ee38ecfe6c688059e8156f8edc9a
Nicolas Lurkin <nicolas.lurkin@b12-consulting.com>
2023-12-05 23:11:04 +0100

    Migrate to telegram bot 20 with job queue

--- webserver/waterdash/PlantAlertBot.py
+++ webserver/waterdash/PlantAlertBot.py
@@ -2,72 +2,113 @@
 
 import logging
 import sys
-import sched
 import time
-from datetime import datetime, timedelta
+from datetime import time
 
+import pytz
 from telegram import Update
-from telegram.ext import (CallbackContext, CommandHandler, PicklePersistence,
-                          Updater)
+from telegram.ext import (ApplicationBuilder, CallbackContext, CommandHandler,
+                          JobQueue, PicklePersistence)
+
+from data.config import owm_city, owm_token, telegram_token
 from owm import owm_wrapper
-from data.config import owm_token, owm_city, telegram_token
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-def start(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+###########################
+# User management functions
+###########################
+def ensure_registered_users(context):
+    if "RegisteredUsers" not in context.bot_data:
+        context.bot_data["RegisteredUsers"] = []
 
 
 def add_user(context, chat_id):
-    if not "RegisteredUsers" in context.bot_data:
-        context.bot_data["RegisteredUsers"] = []
+    ensure_registered_users(context)
     context.bot_data["RegisteredUsers"].append(chat_id)
 
-def register(update: Update, context: CallbackContext):
+
+##############
+# Bot commands
+##############
+async def start(update: Update, context: CallbackContext):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
+async def register(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     add_user(context, chat_id)
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Thank you for registering")
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="Thank you for registering")
 
-def am_i_registered(update: Update, context: CallbackContext):
+
+async def am_i_registered(update: Update, context: CallbackContext):
+    ensure_registered_users(context)
     chat_id = update.effective_chat.id
     if chat_id in context.bot_data["RegisteredUsers"]:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="I'm not forgetting about you!")
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id, text="I'm not forgetting about you!")
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, who are you?")
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id, text="Sorry, who are you?")
 
 
+async def inform_clients(context: CallbackContext, message: str):
+    ensure_registered_users(context)
+    for chat in context.bot_data["RegisteredUsers"]:
+        await context.bot.send_message(chat_id=chat, text=message)
+
+
+################
+# Initialization
+################
 def init_bot():
-    my_persistence = PicklePersistence(filename='PlantAlertBot')
-    updater = Updater(token=telegram_token, persistence=my_persistence)
-
-    dispatcher = updater.dispatcher
+    my_persistence = PicklePersistence(filepath='PlantAlertBot')
+    application = ApplicationBuilder().token(
+        telegram_token).persistence(my_persistence).build()
 
     handler = CommandHandler('start', start)
-    dispatcher.add_handler(handler)
+    application.add_handler(handler)
 
     handler = CommandHandler('register', register)
-    dispatcher.add_handler(handler)
+    application.add_handler(handler)
 
     handler = CommandHandler('amiregistered', am_i_registered)
-    dispatcher.add_handler(handler)
+    application.add_handler(handler)
 
-    if not "RegisteredUsers" in dispatcher.bot_data:
-        dispatcher.bot_data["RegisteredUsers"] = []
+    return application
 
-    return dispatcher, updater
 
+async def start_bot(application):
+    await application.initialize()
+    await application.start()
+
+    if "RegisteredUsers" not in application.bot_data:
+        application.bot_data["RegisteredUsers"] = []
+
+    await application.updater.start_polling()
+
+
+#####################
+# Open Weather checks
+#####################
 def check_value_alerts(values, low_levels, high_levels, val_name, unit):
     min_val = min([_[0] for _ in values])
     max_val = max([_[0] for _ in values])
     for l in low_levels:
-        alerts = [_ for _ in values if _[0]<l]
-        if len(alerts)>0:
-            return f"WARNING: {val_name} below {l}{unit} foreseen starting at {alerts[0][1]}, with a minimum of {min_val}"
+        alerts = [_ for _ in values if _[0] < l]
+        bxl_time = alerts[0][1].astimezone(pytz.timezone('Europe/Brussels'))
+        if len(alerts) > 0:
+            return f"WARNING: {val_name} below {l}{unit} foreseen starting at {bxl_time}, with a minimum of {min_val}"
 
     for l in high_levels:
-        alerts = [_ for _ in values if _[0]>l]
-        if len(alerts)>0:
-            return f"WARNING: {val_name} above {l}{unit} foreseen starting at {alerts[0][1]}, with a maximum of {max_val}"
+        alerts = [_ for _ in values if _[0] > l]
+        if len(alerts) > 0:
+            bxl_time = alerts[0][1].astimezone(
+                pytz.timezone('Europe/Brussels'))
+            return f"WARNING: {val_name} above {l}{unit} foreseen starting at {bxl_time}, with a maximum of {max_val}"
 
     return None
 
@@ -77,54 +118,45 @@ def check_conditions(owm):
     hourly = owm.prepare_hourly_12h_forecast()
     message_list = []
 
-    temps = [(_[1].temperature("celsius")["temp"],_[0]) for _ in hourly]
-    message = check_value_alerts(temps, [0], [30, 20], "Temperature", "°C")
+    temps = [(_[1].temperature("celsius")["temp"], _[0]) for _ in hourly]
+    message = check_value_alerts(temps, [5], [30, 20], "Temperature", "°C")
     if message is not None:
         message_list.append(message)
 
-    gusts = [(_[1].wind()["gust"]*3.6, _[0]) for _ in hourly] # In km/h
+    gusts = [(_[1].wind()["gust"]*3.6, _[0]) for _ in hourly]  # In km/h
     message = check_value_alerts(gusts, [], [60, 80], "Wind gusts", "kph")
     if message is not None:
         message_list.append(message)
 
     return message_list
 
-def inform_clients(updater, dispatcher, message):
-    for chat in dispatcher.bot_data["RegisteredUsers"]:
-        updater.bot.send_message(chat_id=chat, text=message)
 
-def scheduled_run(s, owm, updater, dispatcher):
+async def scheduled_run(context):
+    owm = context.job.data['owm']
+
     message_list = check_conditions(owm)
     for message in message_list:
-        inform_clients(updater, dispatcher, message)
+        await inform_clients(context, message)
 
-    next_run = datetime.now()
-    curr_hour = int(next_run.hour/8)*8
-    next_run = next_run.replace(hour=curr_hour, minute=0, second=0) + timedelta(hours=8)
 
-    s.enterabs(next_run.timestamp(), 0, lambda : scheduled_run(s, owm, updater, dispatcher))
+def init_owm_job(job_queue: JobQueue):
+    from mongodb import from_utc, to_utc
+    owm = owm_wrapper(owm_token, owm_city)
+    for i in range(0, 10, 1):
+        job_queue.run_daily(scheduled_run, time=time(
+            23, i, 0, tzinfo=pytz.timezone('Europe/Brussels')), data={'owm': owm})
+
 
 def main():
-    owm = owm_wrapper(owm_token, owm_city)
 
-    dispatcher, updater = init_bot()
+    application = init_bot()
 
-    updater.start_polling()
+    init_owm_job(application.job_queue)
 
-    # Init next run (next multiple of 8h)
-    next_run = datetime.now()
-    curr_hour = int(next_run.hour/8)*8
-    next_run = next_run.replace(hour=curr_hour, minute=0, second=0) + timedelta(hours=8)
-
-    s = sched.scheduler(time.time, time.sleep)
-    s.enterabs(next_run.timestamp(), 0, lambda : scheduled_run(s, owm, updater, dispatcher))
-    while True:
-        try:
-            s.run()
-        except KeyboardInterrupt:
-            break
+    application.run_polling()
 
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
